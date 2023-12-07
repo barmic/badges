05ffb6ecd8453d89edba83a5c044b32ea1a98714
Michel Barret <michel.barret@gmail.com>
2023-12-07 23:07:04 +0100

    wip

--- src/badge-parsing.ts
+++ src/badge-parsing.ts
@@ -1,40 +1,49 @@
 import { BadgeParams, BadgeType } from './models';
 
-type CommandLine = Record<'cmd' | 'type' | 'firstname' | 'lastname' | 'barcode', number>;
+type CommandLine = Record<'cmd' | 'type' | 'firstname' | 'lastname' | 'barcode', string>;
 
 export function parseBadges(csv: string[][], columns: Record<'cmd' | 'type' | 'firstname' | 'lastname' | 'barcode', number>): BadgeParams[] {
-    const commands: Map<string, BadgeParams> = csv.reduce(
-      (a: Map<string, BadgeParams>, b: string[]) => {
-        const cmd = b[columns.cmd]
-        const tarif = b[columns.type];
-        const univ1 = tarif.startsWith('Uam') ? tarif.substring(7) : undefined;
-        const univ2 = tarif.startsWith('Upm') ? tarif.substring(7) : undefined;
-        const type = typeMapping(tarif);
-        const current = a.get(cmd);
-        if (current) {
-          if (univ1) {
-            current.univ1 = univ1;
-          } else if (univ2) {
-            current.univ2 = univ2;
-          } else {
-            current.type = type;
-          }
-        } else {
-          a.set(cmd, {
+     const commands = csv.reduce(
+      (a: Map<string, CommandLine[]>, b: string[]) => {
+        const current = a.get(b[columns.cmd]) ?? [];
+        current.push({
+            cmd: b[columns.cmd],
+            type: b[columns.type],
             firstname: b[columns.firstname],
-            lastname: b[columns.lastname],
-            type: type,
+            lastname:b[columns.lastname],
             barcode: b[columns.barcode],
-            univ1: univ1,
-            univ2: univ2,
-          })
-        }
+        });
+        a.set(b[columns.cmd], current);
         return a;
       },
-      new Map<string, BadgeParams>()
+      new Map<string, CommandLine[]>()
     );
 
-    return Array.from(commands.values());
+    return Array.from(commands.values()).flatMap((cmd) => commandsBadges(cmd));
+}
+
+function commandsBadges(command: CommandLine[]): BadgeParams[] {
+    return command
+        .filter((line) => MAPPING.has(line.type))
+        .map((line) => ({
+            ...line,
+            type: MAPPING.get(line.type),
+            univ1: foundUniv('Uam', command, line),
+            univ2: foundUniv('Upm', command, line),
+        } satisfies BadgeParams));
+}
+
+function foundUniv(ampm: 'Uam' | 'Upm', lines: CommandLine[], command: CommandLine): string | undefined {
+    if (command.type !== 'Université et Conférence') {
+        return undefined;
+    }
+    let found: string | undefined = undefined;
+    for (const line of lines) {
+        if (line.type.startsWith(ampm)) {
+            found = line.type.substring(7);
+        }
+    }
+    return found;
 }
 
 const MAPPING = new Map<string, BadgeType>([
